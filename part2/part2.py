0ce3beb5c191e6083a45395cdd5c82b5ffd9185b
Amer Sulieman <asulieman@hedrick-2.local>
2019-09-09 21:45:21 -0500

    random binary is generated, 21 bits but 20 of them are used

--- part2/part2.py
+++ part2/part2.py
@@ -5,13 +5,35 @@ x = np.loadtxt("../HousingData/X.txt")
 y = np.loadtxt("../HousingData/Y.txt")
 
 def generate_random_bits_vector():
-    beta_bits = 19
-    victor_bits = 19 *4
-    random_number = []
+
+    number_of_betas = 4
+    bits_per_beta = 20
+    entire_vector_size = number_of_betas * bits_per_beta
+    random_betas= []
     low = 0
-    high = 2**20
-    for x in range(4):
-        rand_int = int((random.randint(low,high+1)-(2**19))/10)
-        random_number.append(bin(rand_int))
-    bin_arr = np.array(random_number)
+    high = 2**bits_per_beta
+    for beta in range(number_of_betas):
+        # the following provides a range for rang between (-2^19 - 2^19)
+        # Because my entire beta vector size is 2^20 bits combos
+        random_number = random.randint(low, high+1) - (2**19)
+        random_betas.append(format(random_number,"021b"))
+    #format the binary with w1 bits, one will be used for negative sign
+    # if there is no negative, anyway i will be only using 20 bits and ignoring 
+    #first one example-> -1011 - > i will use 1011 for neighbors finding
+    #                    01110 -> i will use 1110 for neighbors finding
+    bin_arr = np.array(random_betas)
     return bin_arr
+
+vector = generate_random_bits_vector()
+print(vector)
+# def calculate_MSE():
+
+def calc_MSE(x, y, B, num_rows):
+    #RSS = (Y - Y')^2 but since we can't do square, we use transpose multiplication
+    error = (y - x @ B ).conjugate().transpose() @ (y - x @ B)
+    MSE = error/num_rows
+    #Now we can take the root of our MSE since we squared it to avoid erros
+    root_MSE = MSE **0.5 
+    return root_MSE
+
+
