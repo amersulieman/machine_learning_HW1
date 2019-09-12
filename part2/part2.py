d4a9f63bfd9278bac60c3255807e33dbc0e6550c
Amer Sulieman <asulieman@hedrick-2.local>
2019-09-11 22:14:43 -0500

    refactored but more cleaning needed

--- part2/part2.py
+++ part2/part2.py
@@ -1,57 +1,5 @@
 import numpy as np
 import random
-import time
-
-
-def generate_random_bits_vector():
-    betas_vector = []
-    for beta in range(number_of_betas):
-        random_number = np.random.choice([0, 1], size=(20))
-        betas_vector.append(list(random_number))
-    return betas_vector
-
-
-def convert_binary_betas_to_numbers(betas_vector):
-    # holds the final betas
-    betas = []
-    for binary_beta in betas_vector:
-        # concatenate the binaries to be treated as one binary number
-        string_betas = "".join(map(str, binary_beta))
-        # convert the base 2 binary number to integer
-        beta = int(string_betas, 2)
-        # subtract by 2^19 and divide by 10 to set the range i decided for my betas
-        beta -= (2**19)
-        beta /= 10
-        betas.append(beta)
-    return betas
-
-
-def calc_MSE(x, y, betas, num_rows):
-    B = convert_binary_betas_to_numbers(betas)
-    B = np.array(B)  # convert it to numpy array
-    # RSS = (Y - Y')^2 but since we can't do square, we use transpose multiplication
-    error = (y - x @ B).conjugate().transpose() @ (y - x @ B)
-    MSE = error/num_rows
-    # Now we can take the root of our MSE since we squared it to avoid erros
-    root_MSE = MSE ** 0.5
-    return root_MSE
-
-
-def get_neighbor_and_its_fitness(betas_vector):
-    '''
-        A function that finds all the neighbors of a vector
-        If a vector size is 80 bits, then there are 80 
-        neighbors since at a time we can only change one bit
-    '''
-    for beta in range(number_of_betas):
-        for bit in range(bits_per_beta):
-            new_beta = betas_vector[beta][:]
-            new_beta[bit] ^= 1
-            neighbor = betas_vector[:]
-            neighbor[beta] = new_beta
-            neighbor_fitness = calc_MSE(x, y, neighbor, num_rows)
-            yield neighbor, neighbor_fitness
-
 
 # load the data of each file
 x = np.loadtxt("../HousingData/X.txt")
@@ -62,23 +10,61 @@ entire_vector_size = number_of_betas * bits_per_beta
 num_rows, _ = np.shape(x)
 
 
-iterations = 10
-begin = time.time()
-generation = 30000
+def generate_random_betas_vector():
+    betas_vector = []
+    for beta in range(number_of_betas):
+        random_number = random.randint(0, 2**bits_per_beta)
+        betas_vector.append(random_number)
+    return betas_vector
+
+
+def calc_MSE(x, y, beta_vector, num_rows):
+    # reduce each beta to fall into a specified range
+    # -(2^20)/10 to (2^20)/10
+    B = [(beta - 2**19)/10 for beta in beta_vector]
+    B = np.array(B)
+    # RSS = (Y - Y')^2 but since we can't do square, we use transpose multiplication
+    error = (y - x @ B).conjugate().transpose() @ (y - x @ B)
+    MSE = error/num_rows
+    # Now we can take the root of our MSE since we squared it to avoid erros
+    root_MSE = MSE ** 0.5
+    return root_MSE
+
+
+def get_neighbor(current_betas_vector):
+    '''
+        A function that finds all the neighbors of a vector
+        If a vector size is 80 bits, then there are 80
+        neighbors since at a time we can only change one bit
+    '''
+    iteration = 0
+    for old_beta in current_betas_vector:
+        for bit in range(bits_per_beta):
+            # shift 1 per bit so only one bit per neighbor changes
+            bit_mask = 1 << bit
+            new_beta = old_beta
+            new_beta ^= bit_mask
+            neighbor = current_betas_vector[:]
+            neighbor[iteration] = new_beta
+            yield neighbor
+        iteration += 1
+
+
+generation = 3000
 restart = 100
-all_mses = []
+best_mse = 2**22
 for value in range(restart):
     local_min = False
-    current_beta_vector = generate_random_bits_vector()
+    current_beta_vector = generate_random_betas_vector()
     current_mse = calc_MSE(x, y, current_beta_vector, num_rows)
     generation_count = 1
     while generation_count <= generation and local_min == False:
         found_better_neighbor = False
         # print("generation count --> ", generation_count, "\n")
-        neighbor_generator = get_neighbor_and_its_fitness(
-            current_beta_vector)
+        neighbor_generator = get_neighbor(current_beta_vector)
         for bit in range(entire_vector_size):
-            neighbor, neighbor_fitness = next(neighbor_generator)
+            neighbor = next(neighbor_generator)
+            neighbor_fitness = calc_MSE(x, y, neighbor, num_rows)
             if neighbor_fitness < current_mse:
                 current_beta_vector = neighbor
                 current_mse = neighbor_fitness
@@ -88,7 +74,7 @@ for value in range(restart):
             # print("Betas --> ", convert_binary_betas_to_numbers(current_beta_vector))
             # print("Local MSE--> ", current_mse)
             local_min = True
+        if current_mse < best_mse:
+            best_mse = current_mse
         generation_count += 1
-        all_mses.append(current_mse)
-end = time.time()
-print("speed -> ", end-begin)
+print(best_mse)
